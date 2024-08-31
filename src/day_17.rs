87753c42089bfce0f14b86990d1ae90669ca2fed
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-31 15:30:29 +0200

    aoc17: pt2 done 2 versions

--- src/day_17.rs
+++ src/day_17.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+use std::collections::HashMap;
 use crate::array_2d::Coordinate;
 
 pub fn part_1(input: &str) -> isize {
@@ -9,8 +11,39 @@ pub fn part_1(input: &str) -> isize {
     (best_v_y.pow(2) + best_v_y) / 2
 }
 
-pub fn part_2(input: &str) -> u128 {
-    0
+pub fn part_2(input: &str) -> isize {
+    let area = Area2D::from_input(input);
+
+    // Must not overshoot in one step
+    let max_vx = area.lower_right.x();
+
+    // Must not stall the x coordinate of the probe before reaching the left edge of the target
+    let min_vx = ((-1.0 + f64::sqrt(1.0 + 8.0 * area.upper_left.x() as f64)) / 2.0).ceil() as isize;
+
+    // Must not "overshoot" during (i.e., use same v_y as in pt1)
+    let max_vy = -area.lower_right.y() - 1;
+
+    // Must not "overshoot" in one step
+    let min_vy = area.lower_right.y();
+
+    let mut num_results = 0;
+
+    // Memo version - slower
+    // let mut probe = Probe::from_area(&area);
+    // for vx in min_vx..=max_vx {
+    //     for vy in min_vy..=max_vy {
+    //         num_results += probe.will_hit_target(0, 0, vx, vy);
+    //     }
+    // }
+
+    // Naive version - faster
+    for vx in min_vx..=max_vx {
+        for vy in min_vy..=max_vy {
+            num_results += simulate(0, 0, vx, vy, &area);
+        }
+    }
+
+    num_results
 }
 
 struct Area2D {
@@ -20,8 +53,9 @@ struct Area2D {
 
 #[allow(dead_code)]
 impl Area2D {
-    pub fn point_within(&self, point: Coordinate) -> bool {
-        self.upper_left.x() < point.x() && point.x() < self.lower_right.x() && point.y() < self.upper_left.y() && self.lower_right.y() < point.y()
+
+    pub fn coordinates_within(&self, x: isize, y: isize) -> bool {
+        self.upper_left.x() <= x && x <= self.lower_right.x() && y <= self.upper_left.y() && self.lower_right.y() <= y
     }
 
     pub fn from_input(input_str: &str) -> Self {
@@ -36,6 +70,53 @@ impl Area2D {
     }
 }
 
+fn simulate(mut x: isize, mut y: isize, mut vx: isize, mut vy: isize, target_area: &Area2D) -> isize{
+    while !((x > target_area.lower_right.x()) || (y < target_area.lower_right.y() && vy <= 0) || (x < target_area.upper_left.x() && vx == 0)) {
+        if target_area.coordinates_within(x, y) { return 1 }
+        x += vx;
+        y += vy;
+        vx = max(0, vx - 1);
+        vy -= 1;
+    }
+    0
+}
+
+#[allow(dead_code)]
+struct Probe<'a> {
+    target_area: &'a Area2D,
+    memo: HashMap<[isize; 4], isize>,  // x, y, vx, vy
+}
+
+#[allow(dead_code)]
+impl<'a> Probe<'a> {
+
+    pub fn from_area(area: &'a Area2D) -> Self {
+        Probe {
+            target_area: area,
+            memo: HashMap::new(),
+        }
+    }
+
+    pub fn will_hit_target(&mut self, x: isize, y: isize, vx: isize, vy: isize) -> isize {
+        if let Some(&memo_value) = self.memo.get(&[x, y, vx, vy]) {
+            return memo_value
+        }
+
+        if self.target_area.coordinates_within(x, y) {
+            return 1
+        }
+
+        if (x > self.target_area.lower_right.x()) || (y < self.target_area.lower_right.y() && vy <= 0) || (x < self.target_area.upper_left.x() && vx == 0) {
+            self.memo.insert([x, y, vx, vy], 0);
+            return 0
+        }
+
+        let res = self.will_hit_target(x + vx, y + vy, max(0, vx - 1), vy - 1);
+        self.memo.insert([x, y, vx, vy], res);
+        res
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,10 +124,16 @@ mod tests {
     #[test]
     fn aoc_examples_work() {
         assert_eq!(part_1("target area: x=20..30, y=-10..-5"), 45);
+        assert_eq!(part_2("target area: x=20..30, y=-10..-5"), 112);
     }
 
     #[test]
-    fn aoc_examples_work() {
-        assert_eq!(part_1("target area: x=257..286, y=-101..-57"), 5050);
+    fn pt_1_works() {
+        assert_eq!(part_1("target area: x=257..286, y=-101..-57"), 5_050);
+    }
+
+    #[test]
+    fn pt_2_works() {
+        assert_eq!(part_2("target area: x=257..286, y=-101..-57"), 2_223);
     }
 }
